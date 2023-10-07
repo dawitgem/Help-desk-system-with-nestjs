e98cec9623501a840688768a12aff6fafc270395
dawit2001 <abebedawit2001@gmail.com>
2023-10-07 19:37:46 +0300

    fix error for backend

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import * as cors from 'cors';
 
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors();
+  app.use(cors());
   await app.listen(8000);
 }
 bootstrap();

771da8c1623fdc3eb7ce0d5f1046c4dd73ec3cb0
dawit2001 <abebedawit2001@gmail.com>
2023-10-07 23:42:16 +0300

    new commit

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
-import * as cors from 'cors';
 
 async function bootstrap() {
   dotenv.config();
-  const app = await NestFactory.create(AppModule, { cors: true });
-  app.use(cors());
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://kns-support-api-dawit2001.vercel.app',
+      'https://kns-support.vercel.app',
+    ],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Content-Range', 'X-Content-Range'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
   await app.listen(8000);
 }
 bootstrap();

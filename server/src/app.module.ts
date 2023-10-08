cee071eb6a4d344c08ab52e29fcca735b98bc2e3
dawit2001 <abebedawit2001@gmail.com>
2023-10-08 13:12:44 +0300

    configure cors test

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
@@ -9,10 +9,27 @@ import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
 import { UserService } from './user/user.service';
 import { PassportModule } from '@nestjs/passport';
+import * as express from 'express';
 
 @Module({
   imports: [UserModule, AuthModule, PassportModule],
   controllers: [AppController, UserController, AuthController],
   providers: [AppService, PrismaService, AuthService, UserService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(express.json(), express.urlencoded({ extended: true }))
+      .forRoutes('*');
+    consumer
+      .apply((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header(
+          'Access-Control-Allow-Headers',
+          'Origin, X-Requested-With, Content-Type, Accept',
+        );
+        next();
+      })
+      .forRoutes('*');
+  }
+}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,24 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://kns-support-api-dawit2001.vercel.app',
-      'https://kns-support.vercel.app',
-    ],
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    exposedHeaders: ['Content-Range', 'X-Content-Range'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  });
-
   await app.listen(8000);
 }
 bootstrap();
